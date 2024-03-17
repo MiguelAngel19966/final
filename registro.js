705a6625916020b1782ced6c32fc0843f5421bb0
MiguelAngel199660 <mangel.1996@hotmail.com>
2024-03-17 16:20:03 -0500

    code improved

--- registro.js
+++ registro.js
@@ -20,20 +20,21 @@ class Formulario {
   agregarPelicula(event) {
     event.preventDefault(); 
 
-
     const nombre = document.getElementById("validationDefault01").value;
     const director = document.getElementById("validationDefault02").value;
     const anioEstreno = document.getElementById("validationDefaultUsername").value;
     const genero = document.getElementById("dropdown").value;
 
-
     const pelicula = new Pelicula(nombre, director, anioEstreno, genero);
     this.peliculas.push(pelicula); 
 
-
+    this.mostrarPelicula(pelicula);
     this.guardarPeliculas();
+    this.form.reset();
+    this.agregarEventosEliminar(); 
+  }
 
-
+  mostrarPelicula(pelicula) {
     const movieInfo = `
       <div class="movie-info" style="color: white;" data-id="${pelicula.id}">
         <p><strong>Nombre de la Película:</strong> ${pelicula.nombre}</p>
@@ -45,26 +46,7 @@ class Formulario {
       </div>
     `;
 
-
     this.movieContainer.insertAdjacentHTML("beforeend", movieInfo);
-
-
-    this.form.reset();
-
-
-    const deleteButtons = document.querySelectorAll(".delete-btn");
-    deleteButtons.forEach((button) => {
-      button.addEventListener("click", () => {
-
-        const id = parseInt(button.parentElement.dataset.id);
-
-        button.parentElement.remove();
-
-        this.peliculas = this.peliculas.filter(pelicula => pelicula.id !== id);
-
-        this.guardarPeliculas();
-      });
-    });
   }
 
   guardarPeliculas() {
@@ -73,36 +55,23 @@ class Formulario {
 
   mostrarPeliculas() {
     this.peliculas.forEach(pelicula => {
-      const movieInfo = `
-        <div class="movie-info" style="color: white;" data-id="${pelicula.id}">
-          <p><strong>Nombre de la Película:</strong> ${pelicula.nombre}</p>
-          <p><strong>Director:</strong> ${pelicula.director}</p>
-          <p><strong>Año de Estreno:</strong> ${pelicula.añoEstreno}</p>
-          <p><strong>Género:</strong> ${pelicula.genero}</p>
-          <button class="btn btn-danger delete-btn">Eliminar</button>
-          <hr>
-        </div>
-      `;
-  
-      this.movieContainer.insertAdjacentHTML("beforeend", movieInfo);
+      this.mostrarPelicula(pelicula);
     });
 
+    this.agregarEventosEliminar(); 
+  }
 
+  agregarEventosEliminar() {
     const deleteButtons = document.querySelectorAll(".delete-btn");
     deleteButtons.forEach((button) => {
       button.addEventListener("click", () => {
-
         const id = parseInt(button.parentElement.dataset.id);
-
         button.parentElement.remove();
-
         this.peliculas = this.peliculas.filter(pelicula => pelicula.id !== id);
-        // Guardar las películas actualizadas en localStorage
         this.guardarPeliculas();
       });
     });
   }
 }
-
 
 const formulario = new Formulario();
